cd16ad5d6bb294b2411ef78a301500071c7e20c1
ngducduyyy <ngducduyyy@gmail.com>
2023-11-27 01:36:24 +0700

    new update

--- trailer-plugin/java.js
+++ trailer-plugin/java.js
@@ -2,11 +2,14 @@ const btn = document.querySelector(".btn-plugin")
 const closeIcon = document.querySelector(".close-btn")
 const trailer = document.querySelector(".trailer")
 const video = document.querySelector("video")
+const Nav3 = document.querySelector(".main-header")
 btn.addEventListener("click",()=>{
     trailer.classList.remove("active");
+    Nav3.classList.add("hide")
 });
 closeIcon.addEventListener("click", ()=>{
     trailer.classList.add("active");
+    Nav3.classList.remove("hide");
     video.pause();
     video.currentTime = 0;
 })
